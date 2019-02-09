cd74341439dd665f142dde7a6893d1e46187be9a
endyrinho <nakhanov.dias@gmail.com>
2019-02-09 11:48:04 +0600

    week2

---
+++ Week1/Task1/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        public static bool getStatus(string myString)
+        {
+            string first = myString.Substring(0, myString.Length / 2);
+            char[] arr = myString.ToCharArray();
+
+            Array.Reverse(arr);
+
+            string temp = new string(arr);
+            string second = temp.Substring(0, temp.Length / 2);
+
+            return first.Equals(second);
+        }
+    }
---
+++ Week2/Task1/Task1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+
+    {
+        public static bool getStatus(string myString)
+        {
+            int length = myString.Length;
+            for (int i = 0; i < length / 2; i++)
+            {
+
+                if (myString[i] != myString[length - i - 1])
+                    return false;
+            }
+            return true;
+
+
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            if (getStatus(s) == true)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+ 
+
